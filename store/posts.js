1a35b2d96b1af298b84a29e1aeeb5df09eb98aaf
Andre Liem <andre.liem@gmail.com>
2017-06-28 12:04:00 -0700

    more posts

--- store/posts.js
+++ store/posts.js
@@ -5,6 +5,18 @@ export const state = () => ({
       slug: 'getting-started-nuxt-markdown',
       title: 'Getting Started with Nuxt + MarkDown',
       created_at: '06/27/2017'
+    },
+    {
+      id: 2,
+      slug: 'my-top-10-favourites-of-being-a-freelancer-software-developer',
+      title: 'My top 10 favourites of being a freelance software developer',
+      created_at: '10/18/2016'
+    },
+    {
+      id: 3,
+      slug: 'setting-up-php-xdebug-with-laravel-vagrant-and-phpstorm',
+      title: 'Setting up PHP xdebug with Laravel, Vagrant, and PHPStorm',
+      created_at: '15/18/2016'
     }
   ]
 })
